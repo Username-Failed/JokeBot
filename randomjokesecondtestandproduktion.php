dc83ae7de51cab087e05786434cc9d586e0bd814
Username-Failed <marcuslyngbyekolbe@gmail.com>
2017-05-02 10:35:27 +0200

    copy of the randomjokesecond
    made for newer systems

---
+++ randomjokesecondtestandproduktion.php
@@ -0,0 +1,68 @@
+<?php
+
+    function testForLastJoke($jokeId, $lastJoke, $jokeArray) {
+        if ($jokeId == $lastJoke['lastJoke']) {
+            $jokeId = array_rand($jokeArray, 1);
+            //echo "Value Changed <br />";
+            $jokeId = testForLastJoke($jokeId, $lastJoke, $jokeArray);
+        }
+        return $jokeId;
+    }
+
+    session_start();
+
+    /*
+        Edit the config.php.template file before you can use this script
+    */
+    $configDone = false;
+
+    include("config.php");
+
+    if($configDone == false) {
+        echo "Error: Config File not made";
+        exit();
+    }
+
+    if ($dbc = mysqli_connect(DBHOST, DBUSER, DBPW)) {
+
+        if(!mysqli_select_db(DBNAME)) {
+            trigger_error("Could not select the database!<br />MySQL Error: " . mysql_error());
+            exit();
+        }
+    } else {
+        trigger_error("Could not connect to MySQL!<br />MySQL Error: " . mysql_error());
+        exit();
+    }
+
+    $jokeResult = mysqli_query("SELECT * FROM chuck");
+
+    $array = array();
+
+    while($row = mysqli_fetch_array($jokeResult)) {
+        $array[] = $row["joke"];
+        //echo $row["joke"];
+    }
+
+    $key = array_rand($array, 1);
+
+    $key = testForLastJoke($key, $_SESSION['lastJoke'], $array);
+
+    echo $array[$key];
+
+    $_SESSION['lastJoke'] = $key;
+
+    /*
+    if (mysql_num_rows($jokeResult) > 0) {
+        // output data of each row
+        while($row = mysql_fetch_assoc($jokeResult)) {
+            echo "id: " . $row["id"]. " - Joke: " . $row["joke"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    */
+    mysqli_close($dbc);
+?>
+
+<br />
+<a href="randomjokesecond.php">Click to get a new joke</a>
